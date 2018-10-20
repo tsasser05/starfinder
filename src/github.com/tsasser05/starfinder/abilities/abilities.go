db1d3f2a0b92ac9c4e86abab33a3119a9a6875b1
tsasser05 <tsasser05@users.noreply.github.com>
2018-10-20 15:35:18 -0400

    Fixed formatting in Abilities Display()

--- src/github.com/tsasser05/starfinder/abilities/abilities.go
+++ src/github.com/tsasser05/starfinder/abilities/abilities.go
@@ -28,11 +28,11 @@ func (a *Abilities) Roll() {
 
 func (a *Abilities) Display() {
 	fmt.Println("Abilities")
-	fmt.Printf("Strength:  %d\n", a.Strength)
-	fmt.Printf("Dexterity:  %d\n", a.Dexterity)
-	fmt.Printf("Constitution:  %d\n", a.Constitution)
-	fmt.Printf("Intelligence:  %d\n", a.Intelligence)
-	fmt.Printf("Wisdom:  %d\n", a.Wisdom)
-	fmt.Printf("Charisma:  %d\n", a.Charisma)
+	fmt.Printf("    Strength:  %d\n", a.Strength)
+	fmt.Printf("    Dexterity:  %d\n", a.Dexterity)
+	fmt.Printf("    Constitution:  %d\n", a.Constitution)
+	fmt.Printf("    Intelligence:  %d\n", a.Intelligence)
+	fmt.Printf("    Wisdom:  %d\n", a.Wisdom)
+	fmt.Printf("    Charisma:  %d\n", a.Charisma)
 
 } // display()
